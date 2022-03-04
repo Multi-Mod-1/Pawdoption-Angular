66f030db20eeec161a84edbf0a0cd773d9a8648f
LuFrei <lucasfreitas09876@gmail.com>
2022-03-04 11:55:17 -0500

    set up directory and files for all dogs page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'form', component: AdoptionFormComponent}
+  // {path: 'form', component: AdoptionFormComponent},
 ];
 
 @NgModule({
