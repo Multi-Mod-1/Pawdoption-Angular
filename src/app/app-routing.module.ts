28ed5e278f58e712668498863dca236586acf335
Eric Davenport <edavenportjr@google.com>
2022-03-04 17:03:35 -0500

    about page initial w/o css styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DogListComponent } from './dog/dog-list.component';
 import { DogModule } from './dog/dog.module';
 import { WelcomeComponent } from './home/welcome.compnent';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {path: 'dogs', component: DogListComponent},
   {path: 'welcome', component: WelcomeComponent},
+  {path: 'about', component: AboutComponent},
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+  { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '--', redirectTo: 'about', pathMatch: 'full'}
 ];
 
 @NgModule({
