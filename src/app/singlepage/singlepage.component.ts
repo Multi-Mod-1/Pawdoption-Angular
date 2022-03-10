dbd920bf5ccd6a2fb1bebfa0f1f4919dda8a101b
monterojavier <monterodotjavier@gmail.com>
2022-03-10 14:59:22 -0500

    features to add

---
+++ src/app/singlepage/singlepage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singlepage',
+  templateUrl: './singlepage.component.html',
+  styleUrls: ['./singlepage.component.css'],
+})
+export class SinglePageComponent {
+  title = 'Pawdoption';
+}
