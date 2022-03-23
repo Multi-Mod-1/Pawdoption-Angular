57857207ae04057be77d304d5a4a3d866def6817
LuFrei <lucasfreitas09876@gmail.com>
2022-03-23 12:28:31 -0400

    fixed list shelves not appearing on front end

--- src/app/components/list-shelf/list-shelf.component.ts
+++ src/app/components/list-shelf/list-shelf.component.ts
@@ -2,10 +2,14 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'list-shelf',
-    templateUrl: './list-shelf.component.html'
+    templateUrl: './list-shelf.component.html',
+    styleUrls: ['./list-shelf.component.css']
 })
 
 export class ListShelfComponent {
+    imageWidth: number = 200;
+    imageMargin: number = 2;
+
     @Input() name: string = '';
     @Input() summary: string = '';
     @Input() imagePath: string = '';
