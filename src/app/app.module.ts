85e835dfc04f8c159a3110b9057cd74f451a8dc8
monterojavier <monterodotjavier@gmail.com>
2022-03-23 12:43:39 -0400

    sticky footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationBarComponent],
+  declarations: [AppComponent, NavigationBarComponent, FooterComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
